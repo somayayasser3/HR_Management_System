c96422555bb8b7a624a7a90a0aa770676de5e374
AbdoRagae74 <abdelrahmanragae34@gmail.com>
2025-07-19 17:43:08 +0300

    Update attendance added + DTO + changed checkin and checkout to be TimeSpan

--- HrManagementSystem/Controllers/AttendanceController.cs
+++ HrManagementSystem/Controllers/AttendanceController.cs
@@ -17,7 +17,7 @@ namespace HrManagementSystem.Controllers
     {
         UnitOfWork unit;
         IMapper mapper;
-        public AttendanceController(UnitOfWork u,IMapper map)
+        public AttendanceController(UnitOfWork u, IMapper map)
         {
             unit = u;
             mapper = map;
@@ -39,10 +39,10 @@ namespace HrManagementSystem.Controllers
         public IActionResult GetAttendanceForEmployee(int empid)
         {
             List<GetAttendaceDTO> EmpAttendance = mapper.Map<List<GetAttendaceDTO>>(unit.AttendanceRepo.GetAttendanceForEmployee(empid));
-            if(unit.EmployeeRepo.getByID(empid)==null) return NotFound(new { message = "No such employee" });
+            if (unit.EmployeeRepo.getByID(empid) == null) return NotFound(new { message = "No such employee" });
             if (EmpAttendance.Count == 0)
             {
-                return NotFound(new {message = "Employee doesn't have any attendance" } );
+                return NotFound(new { message = "Employee doesn't have any attendance" });
             }
             return Ok(EmpAttendance);
 
@@ -65,7 +65,7 @@ namespace HrManagementSystem.Controllers
             return Ok(EmpAttendance);
         }
         /// /////////////////////////////////////////
-        
+
 
         [HttpPost("new")]
         [Authorize(Roles = "Admin,HR")]
@@ -73,14 +73,14 @@ namespace HrManagementSystem.Controllers
         {
 
             if (dto == null || dto.CheckInTime >= dto.CheckOutTime)
-                return BadRequest(new{ message = "Invalid check-in or check-out time." });
+                return BadRequest(new { message = "Invalid check-in or check-out time." });
             if (!ModelState.IsValid)
             {
-                    var errors = ModelState
-                                .Where(ms => ms.Value.Errors.Any())
-                                .SelectMany(ms => ms.Value.Errors)
-                                .Select(e => e.ErrorMessage)
-                                .ToList();
+                var errors = ModelState
+                            .Where(ms => ms.Value.Errors.Any())
+                            .SelectMany(ms => ms.Value.Errors)
+                            .Select(e => e.ErrorMessage)
+                            .ToList();
                 return BadRequest(errors);
             }
             TimeSpan requiredCheckIn = new TimeSpan(8, 0, 0); // 8:00 AM
@@ -90,20 +90,20 @@ namespace HrManagementSystem.Controllers
             TimeSpan? actualCheckOut = dto.CheckOutTime;
 
             if (dto.CheckInTime > requiredCheckIn)
-                dto.DelayHours= (decimal)(actualCheckIn - requiredCheckIn).TotalHours;
+                dto.DelayHours = (decimal)(actualCheckIn - requiredCheckIn).TotalHours;
 
             if (dto.CheckOutTime > requiredCheckOut)
                 dto.OvertimeHours = (decimal)(actualCheckOut - requiredCheckOut)?.TotalHours;
-            dto.UpdatedAt= DateTime.Now;
+            dto.UpdatedAt = DateTime.Now;
 
             Attendance newAttendance = mapper.Map<Attendance>(dto);
             try
             {
-            unit.AttendanceRepo.Add(newAttendance);
-            unit.Save();
-            return Ok(new { message = "Attendance added successfully." });
+                unit.AttendanceRepo.Add(newAttendance);
+                unit.Save();
+                return Ok(new { message = "Attendance added successfully." });
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest(new { message = "Try again" });
             }
@@ -114,16 +114,16 @@ namespace HrManagementSystem.Controllers
         {
             Attendance AttendanceToUpdate = unit.AttendanceRepo.getByID(dto.AttendanceId);
 
-            if(AttendanceToUpdate==null) return BadRequest(new { message = "No such attendance" });
+            if (AttendanceToUpdate == null) return BadRequest(new { message = "No such attendance" });
             if (dto == null || dto.CheckInTime >= dto.CheckOutTime)
-                return BadRequest(new{ message = "Invalid check-in or check-out time." });
+                return BadRequest(new { message = "Invalid check-in or check-out time." });
             if (!ModelState.IsValid)
             {
-                    var errors = ModelState
-                                .Where(ms => ms.Value.Errors.Any())
-                                .SelectMany(ms => ms.Value.Errors)
-                                .Select(e => e.ErrorMessage)
-                                .ToList();
+                var errors = ModelState
+                            .Where(ms => ms.Value.Errors.Any())
+                            .SelectMany(ms => ms.Value.Errors)
+                            .Select(e => e.ErrorMessage)
+                            .ToList();
                 return BadRequest(errors);
             }
             TimeSpan requiredCheckIn = new TimeSpan(8, 0, 0); // 8:00 AM
@@ -133,58 +133,41 @@ namespace HrManagementSystem.Controllers
             TimeSpan? actualCheckOut = dto.CheckOutTime;
 
             if (dto.CheckInTime > requiredCheckIn)
-                dto.DelayHours= (decimal)(actualCheckIn - requiredCheckIn).TotalHours;
+                dto.DelayHours = (decimal)(actualCheckIn - requiredCheckIn).TotalHours;
 
             if (dto.CheckOutTime > requiredCheckOut)
                 dto.OvertimeHours = (decimal)(actualCheckOut - requiredCheckOut)?.TotalHours;
+            dto.UpdatedAt = DateTime.Now;
 
-            //dto.CreatedAt = DateTime.Now;
-            dto.UpdatedAt= DateTime.Now;
-
-             mapper.Map(dto,AttendanceToUpdate);
+            mapper.Map(dto, AttendanceToUpdate);
             try
             {
-            unit.AttendanceRepo.Update(AttendanceToUpdate);
-            Attendance newAttendance = mapper.Map<Attendance>(dto);
-            return Ok(new { message = "Attendance added successfully." });
+                unit.AttendanceRepo.Update(AttendanceToUpdate);
+                unit.Save();
+                return Ok(new { message = "Attendance added successfully." });
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest(new { message = "Try again" });
             }
-            unit.AttendanceRepo.Add(newAttendance);
-            Attendance newAttendance = mapper.Map<Attendance>(dto);
-            return Ok("Attendance added successfully.");
-        [Authorize(Roles = "Admin,HR")]
-            
-            unit.AttendanceRepo.Add(newAttendance);
-            
-            Attendance newAttendance = mapper.Map<Attendance>(dto);
-            unit.AttendanceRepo.Add(newAttendance);
-            unit.Save();
-                return BadRequest(new { message = "Wrong ID" });    
-            catch(Exception ex)
-            {
-                return BadRequest();    
-            return Ok(new {message = "Deleted" });
-        [Authorize(Roles = "Admin,HR")]
         }
-            return Ok("Deleted");
+
+
+        [HttpDelete("delete/{id}")]
         [Authorize(Roles = "Admin,HR")]
         public IActionResult DeleteAttendanceRow(int id)
-
-            
+        {
             Attendance attendance = unit.AttendanceRepo.getByID(id);
-            if(attendance == null)
+            if (attendance == null)
             {
-                return BadRequest(new { message = "Wrong ID" });    
+                return BadRequest(new { message = "Wrong ID" });
             }
             unit.AttendanceRepo.Delete(id);
             unit.Save();
-            return Ok(new {message = "Deleted" });
+            return Ok(new { message = "Deleted" });
         }
 
-        
+
 
     }
 }
