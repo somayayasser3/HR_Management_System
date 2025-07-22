87044ae4761d4179a2c23206c8b97e971b0b9829
AbdoRagae74 <abdelrahmanragae34@gmail.com>
2025-07-22 19:00:54 +0300

    EmployeeID in token response

--- HrManagementSystem/DTOs/AuthDTOs/AuthResponseDTO.cs
+++ HrManagementSystem/DTOs/AuthDTOs/AuthResponseDTO.cs
@@ -4,6 +4,7 @@
     {
         public string Token { get; set; }
         public string Email { get; set; }
+        public string EmployeeId { get; set; }
         public string FullName { get; set; }
         public string Role { get; set; }
         public DateTime ExpiresAt { get; set; }
