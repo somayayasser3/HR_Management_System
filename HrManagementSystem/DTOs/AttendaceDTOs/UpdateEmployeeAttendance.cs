029610dee9d8d8b210e9583aedd87800da3748e4
ameragnegm <amera.g.negm@gmail.com>
2025-07-24 21:19:36 +0300

    Add location in Check out

--- HrManagementSystem/DTOs/AttendaceDTOs/UpdateEmployeeAttendance.cs
+++ HrManagementSystem/DTOs/AttendaceDTOs/UpdateEmployeeAttendance.cs
@@ -6,17 +6,16 @@ namespace HrManagementSystem.DTOs.AttendaceDTOs
     public class UpdateEmployeeAttendance
     {
         [Required]
-        public int AttendanceId { get; set; }
-        [Required]
         public int EmployeeId { get; set; }
-        [Required]
-        public DateTime AttendanceDate { get; set; }
-        [Required]
-        [DefaultValue(typeof(TimeSpan), "8:00:00")]
-        public TimeSpan CheckInTime { get; set; }
         [Required]
         [DefaultValue(typeof(TimeSpan), "16:00:00")]
         public TimeSpan? CheckOutTime { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+
 
         //public decimal? OvertimeHours { get; set; }
 
